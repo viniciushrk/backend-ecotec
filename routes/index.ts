bf04c21c386e65e3656ebe42178e3e06919ea841
vinicius rosa <viniciushrk@gmail.com>
2021-06-07 16:15:38 +0000

    init api

--- routes/index.ts
+++ routes/index.ts
@@ -2,6 +2,10 @@ import {Router} from 'express';
 import categoryRouter from './categoryRouter.routes';
 const routes = Router();
 
+routes.get('/', (req,res)=>{
+    return res.send('Tá funfando');
+})
 routes.use('/categorias', categoryRouter)
+
 
 export default routes;
